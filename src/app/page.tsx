7d44d02983efdae9bbd420cc26550441846979dc
taufeeq-ahmed <io.taufeeq.ahmed@gmail.com>
2024-12-29 15:05:28 +0530

    theme issue fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,11 @@
 import About from "@/components/about";
 import Hero from "@/components/hero";
 import NavigationDock from "@/components/navigation-dock";
-import { ThemeToggle } from "@/components/ui/theme-toggle";
 
 export default function Home() {
 
   return (
     <main>
-      <div className="absolute top-4 right-4">
-        <ThemeToggle className="w-12 h-12" />
-      </div>
       <Hero />
       <About />
       <NavigationDock />
