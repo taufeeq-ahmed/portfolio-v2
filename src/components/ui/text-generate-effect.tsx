cf8b4b2b5d3648392394391b7c3a7ae1e85aa13e
taufeeq-ahmed <io.taufeeq.ahmed@gmail.com>
2025-01-01 12:50:43 +0530

    animate on trigger done

--- src/components/ui/text-generate-effect.tsx
+++ src/components/ui/text-generate-effect.tsx
@@ -1,62 +1,91 @@
 "use client";
-import { useEffect } from "react";
-import { motion, stagger, useAnimate } from "framer-motion";
+import { useEffect, useRef } from "react";
+import {
+  motion,
+  stagger,
+  useAnimate,
+  useScroll,
+  useSpring,
+} from "framer-motion";
 import { cn } from "@/lib/utils";
 
 export const TextGenerateEffect = ({
-    words,
-    className,
-    filter = true,
-    duration = 0.5,
+  words,
+  className,
+  filter = true,
+  duration = 0.5,
 }: {
-    words: string;
-    className?: string;
-    filter?: boolean;
-    duration?: number;
+  words: string;
+  className?: string;
+  filter?: boolean;
+  duration?: number;
 }) => {
-    const [scope, animate] = useAnimate();
-    let wordsArray = words.split(" ");
-    useEffect(() => {
-        animate(
-            "span",
-            {
-                opacity: 1,
-                filter: filter ? "blur(0px)" : "none",
-            },
-            {
-                duration: duration ? duration : 1,
-                delay: stagger(0.2),
-            }
-        );
-    }, [scope.current]);
+  const [scope, animate] = useAnimate();
+  let wordsArray = words.split(" ");
 
-    const renderWords = () => {
-        return (
-            <motion.div ref={scope}>
-                {wordsArray.map((word, idx) => {
-                    return (
-                        <motion.span
-                            key={word + idx}
-                            className="text-black opacity-0 text-lg sm:text-4xl "
-                            style={{
-                                filter: filter ? "blur(10px)" : "none",
-                            }}
-                        >
-                            {word}{" "}
-                        </motion.span>
-                    );
-                })}
-            </motion.div>
-        );
-    };
+  const containerRef = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: containerRef,
+    offset: ["start end", "end start"], // Adjust to match when the element enters the viewport
+  });
 
-    return (
-        <div className={cn("font-bold", className)}>
-            <div className="mt-4 p-4 md:p-0">
-                <div className=" dark:text-white text-black text-2xl tracking-wide leading-1 sm:leading-loose">
-                    {renderWords()}
-                </div>
-            </div>
-        </div>
+  const springProgress = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 20,
+  });
+
+  const startAnimation = () => {
+    animate(
+      "span",
+      {
+        opacity: 1,
+        filter: filter ? "blur(0px)" : "none",
+      },
+      {
+        duration: duration ? duration : 1,
+        delay: stagger(0.2),
+      }
     );
+  };
+
+  useEffect(() => {
+    const unsubscribe = scrollYProgress.onChange((value) => {
+      if (value >= 0.1) {
+        startAnimation();
+        unsubscribe();
+      }
+    });
+
+    return unsubscribe;
+  }, [scope.current, scrollYProgress]);
+
+  const renderWords = () => {
+    return (
+      <motion.div ref={scope}>
+        {wordsArray.map((word, idx) => {
+          return (
+            <motion.span
+              key={word + idx}
+              className="text-black opacity-0 text-lg sm:text-4xl "
+              style={{
+                filter: filter ? "blur(10px)" : "none",
+              }}
+            >
+              {word}{" "}
+            </motion.span>
+          );
+        })}
+      </motion.div>
+    );
+  };
+
+  return (
+    <div className={cn("font-bold", className)} ref={containerRef}>
+      <div className="mt-4 p-4 md:p-0">
+        <div className=" dark:text-white text-black text-2xl tracking-wide leading-1 sm:leading-loose">
+          {renderWords()}
+        </div>
+      </div>
+    </div>
+  );
 };
