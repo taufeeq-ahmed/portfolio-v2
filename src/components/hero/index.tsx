548da6208036eb8078d2c1c508ec8baee7c78085
taufeeq-ahmed <io.taufeeq.ahmed@gmail.com>
2025-01-02 11:59:51 +0530

    card width corrected

--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -27,3 +27,4 @@ function Hero({}: Props) {
 }
 
 export default Hero;
+  
--- src/components/ui/animated-cards.tsx
+++ src/components/ui/animated-cards.tsx
@@ -143,7 +143,7 @@ export function AnimatedCards({ cards }: Props) {
           </div>
         ) : null}
       </AnimatePresence>
-      <ul className="max-w-4xl mx-auto w-full grid grid-cols-1 md:grid-cols-2 items-start gap-4">
+      <ul className="max-w-4xl mx-auto w-full grid grid-cols-1 lg:grid-cols-2 items-start gap-4">
         {cards.map((card, index) => (
           <motion.div
             layoutId={`card-${card.title}-${id}`}
